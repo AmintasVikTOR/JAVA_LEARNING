c502d5189db64f8cc429242f8be109e465381455
AmintasVikTOR <amintas.vik@gmail.com>
2019-12-16 01:07:51 +0300

    Loading TASK-3 in GIT(add method-call)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,6 +37,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
+
+        full_out();
+
+    }
+
+    public static void full_out() {
         boolean t_f = true;
 
         do {
--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -1,4 +1,19 @@
+//Here saved task-3
 package com.company;
 
 public class Task3 {
+ public static void main(String[] args) {
+     // call in this class a hometask-2 from Main-class
+
+     //create new object "Mail-class"
+     //i'm use only one method, because this method calls all others methods in the Main-class
+     // and user must make his choice))
+     Main Task3Class=new Main();
+     Task3Class.full_out();
+
+
+
+
+     //System.out.println("Task3");
+ }
 }
