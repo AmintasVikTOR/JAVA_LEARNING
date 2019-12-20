fdd009fbf13f7fcb04c99bf495aee244b17cab39
AmintasVikTOR <amintas.vik@gmail.com>
2019-12-20 09:39:46 +0300

    Start Task4

---
+++ src/com/company/Task4.java
@@ -0,0 +1,10 @@
+//Here saved task-3
+package com.company;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println("HW!!!");
+    }
+}
