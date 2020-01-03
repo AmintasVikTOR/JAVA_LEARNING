10de05d80be866375e91a342cb219f89ff2659b9
AmintasVikTOR <amintas.vik@gmail.com>
2020-01-03 15:28:58 +0300

    Add calculate

---
+++ src/com/company/calculate/IOperations.java
@@ -0,0 +1,8 @@
+package com.company.calculate;
+
+public interface IOperations {
+    double mult(double a, double b);
+    double sum(double a, double b);
+    double minus(double a, double b);
+    double divide(double a, double b);
+}
