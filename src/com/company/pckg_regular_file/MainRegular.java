f9cdf26b92af2a110bdb26d716a20afe6e1f2af2
AmintasVikTOR <amintas.vik@gmail.com>
2020-01-24 17:46:36 +0300

    Modify Regular

--- src/com/company/pckg_regular_file/MainRegular.java
+++ src/com/company/pckg_regular_file/MainRegular.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainRegular {
     public static void main(String[] args) throws IOException {
         //Regular myRegular=new Regular();
         //myRegular.myRegular();
 
-        String s1="ВОЙНА";
+        String s1="война";
 
         FileReader fileReader=new FileReader("C://myfile//full.txt");
         //Scanner scanner=new Scanner(fileReader);
@@ -20,25 +22,36 @@ public class MainRegular {
 
 
         int i=0;
+        int cntWarLine=0;
         int cntWar=0;
         String value="";
+        s1=s1.toUpperCase();
 
-        //Pattern pattern1 = Pattern.compile("Война(\\h*)");
-        //Matcher match1;
+        Pattern pattern1 = Pattern.compile(s1+"(\\h*)");
+
 
         while(fileReader.ready()){
             //строку из файла загоняем в переменную
             String temp=bufferedReader.readLine();
-            String sss=temp.toUpperCase();
-            if (sss.contains(s1))
-                cntWar++;
+
+            for (String s11 : temp.split(" ")){
+                String sss =s11.toUpperCase();
+                Matcher match1 = pattern1.matcher(sss);
+                if (match1.find()) {
+                    cntWar++;
+                }
+//                if (sss.contains(s1))
+//                    cntWar++;
+            }
+
+
         }
 
         //закрываемся...
         bufferedReader.close();
 
 
-        System.out.println("Количество строк со значением 'ВОЙНА'... "+cntWar);
+        System.out.println("Количество слов 'ВОЙНА'... "+cntWar);
 
     }
 }
